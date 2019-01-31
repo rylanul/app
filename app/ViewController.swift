cb5941ce34bac7e59c297a51fc7484b69805e5ae
Rylan Ugalde-Lopez <rylanu1@109d25lx1rwf8j7.student.cssd.ab.ca>
2019-01-31 14:21:57 -0700

    Initial Commit

---
+++ app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  app
+//
+//  Created by Rylan Ugalde-Lopez on 2019-01-31.
+//  Copyright © 2019 Rylan Ugalde-Lopez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
